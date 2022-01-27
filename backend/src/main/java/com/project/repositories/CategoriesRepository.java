992d19cc0496248b0f2e1af275839c3ec7661f96
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-01-27 01:50:49 -0500

    ✨ create new repository to Category entity

---
+++ backend/src/main/java/com/project/repositories/CategoriesRepository.java
@@ -0,0 +1,8 @@
+package com.project.repositories;
+
+import com.project.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriesRepository extends JpaRepository<Category, Long> {}
