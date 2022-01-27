e708d85e33e98a422b6f78464a676f1718b57542
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-01-27 01:50:03 -0500

    🎨 insert new configuration into connect to the database

--- backend/src/main/java/com/project/entities/Category.java
+++ backend/src/main/java/com/project/entities/Category.java
@@ -2,7 +2,17 @@ package com.project.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
 public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
    
