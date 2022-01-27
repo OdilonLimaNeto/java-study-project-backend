45299fe4365836595dd4316b22ceb1c3770d4a4c
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-01-27 01:51:09 -0500

    🎨 insert new service to list all categories

--- backend/src/main/java/com/project/resources/CategoryResource.java
+++ backend/src/main/java/com/project/resources/CategoryResource.java
@@ -1,10 +1,11 @@
 package com.project.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.project.entities.Category;
+import com.project.services.CategoryService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+    @Autowired
+    private CategoryService service;
+
     @GetMapping
     public ResponseEntity<List<Category>> list() {
-        List<Category> allCategories = new ArrayList<>();
-        allCategories.add(new Category(1L, "Games"));
-        allCategories.add(new Category(2L, "Office"));
+        List<Category> allCategories = service.geAlltCategories();
         return ResponseEntity.ok().body(allCategories);
     }
 }
